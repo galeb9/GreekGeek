394a7a6ee661ddb9e1baea3511d804492878a9c1
Matej Grimšič <matej.grimsic@hudlajf.com>
2022-12-25 15:40:44 +0100

    fix: ENV file added for security

--- src/scripts/firebaseInit.js
+++ src/scripts/firebaseInit.js
@@ -5,13 +5,13 @@ import 'firebase/compat/firestore';
 
 // konfiguracija za naš projekt
 const firebaseConfig = {
-    apiKey: "AIzaSyCSc18fYY4jmEXj1Z7laZWcplgISyGBl7I",
-    authDomain: "pushups-150a1.firebaseapp.com",
-    projectId: "pushups-150a1",
-    storageBucket: "pushups-150a1.appspot.com",
-    messagingSenderId: "680008298929",
-    appId: "1:680008298929:web:facbfa24f60e4742cc0d90",
-    measurementId: "G-M72C3CD628"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 // inicijalizacija firebase projekta
