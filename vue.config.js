f2894369f686820fabbc4bc0b89fdb1d02b01432
Matej Grimšič <matej.grimsic@hudlajf.com>
2022-12-27 19:19:00 +0100

    feat: File restored

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    manifestOptions: {
+      name: "Greek Geek",
+      short_name: "GG",
+      icons: [
+        {"src":"./img/icons/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}
+      ],
+      background_color: "#E6EEFA",
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
