170f1478d33fa5cb2f952115ff752412abf04ba9
Matej Grimšič <matej.grimsic@hudlajf.com>
2023-07-04 19:09:14 +0200

    feat: calendar bottom 45% degree box correct placement

--- src/components/home/home-stats/HomeCalendar.vue
+++ src/components/home/home-stats/HomeCalendar.vue
@@ -1,127 +1,132 @@
 <template>
   <div class="calendar-list">
-    <div class="calendar-item"
+    <div
+      class="calendar-item"
       v-for="(el, i) in days"
       :key="i"
-      :class="{active : el.active}"
+      :class="{ active: el.active }"
     >
       <h3 class="char">{{ el.char }}</h3>
       <h3 class="num">{{ el.num }}</h3>
     </div>
   </div>
-
 </template>
 
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
       char: "M",
       num: 3,
-      daysChar: ["S", "M", "T","W", "T", "F", "S"],
+      daysChar: ["S", "M", "T", "W", "T", "F", "S"],
       days: [],
-      today: null
-    }
+      today: null,
+    };
   },
   methods: {
-    daysInMonth (month, year) {
+    daysInMonth(month, year) {
       return new Date(year, month, 0).getDate();
     },
-    getChar(num){
-      return this.daysChar[num]
+    getChar(num) {
+      return this.daysChar[num];
     },
-    getDays(){
-        let todayCharIndex = this.today.getDay(); // day char
-        let todaysDate = this.today.getDate(); //day num
-        let smaller = todayCharIndex;
-        let bigger = 1;
-        const lastMonthLength = this.daysInMonth(this.today.getMonth() , this.today.getFullYear());
-        const currentMonthLength = Number(this.daysInMonth(this.today.getMonth() + 1 , this.today.getFullYear()));
-        let nextMonthDay = 1;
-        let lastMonthDay = lastMonthLength;
+    getDays() {
+      let todayCharIndex = this.today.getDay(); // day char
+      let todaysDate = this.today.getDate(); //day num
+      let smaller = todayCharIndex;
+      let bigger = 1;
+      const lastMonthLength = this.daysInMonth(
+        this.today.getMonth(),
+        this.today.getFullYear()
+      );
+      const currentMonthLength = Number(
+        this.daysInMonth(this.today.getMonth() + 1, this.today.getFullYear())
+      );
+      let nextMonthDay = 1;
+      let lastMonthDay = lastMonthLength;
 
-        for(let i = 0; i < this.daysChar.length; i++){ // nafila koledar z dummy data
-            this.days.push({
-              char: this.getChar(i),
-              num: i,
-              active: false
-            })
+      for (let i = 0; i < this.daysChar.length; i++) {
+        // nafila koledar z dummy data
+        this.days.push({
+          char: this.getChar(i),
+          num: i,
+          active: false,
+        });
+      }
+
+      for (let i = 0; i < this.days.length; i++) {
+        // pohendla day placement
+        let day = this.days[i];
+        if (i === todayCharIndex) {
+          day.num = todaysDate;
+          day.active = true;
+        } else if (i < todayCharIndex) {
+          //smaller
+          day.num = todaysDate - smaller;
+          smaller--;
+        } else if (i > todayCharIndex) {
+          day.num = todaysDate + bigger;
+          bigger++;
         }
+      }
 
-
-        for(let i = 0; i < this.days.length; i++){ // pohendla day placement
-            let day = this.days[i];
-            if(i === todayCharIndex){
-                day.num = todaysDate 
-                day.active = true;
-            }
-            else if(i < todayCharIndex){ //smaller
-                day.num = todaysDate  - smaller;
-                smaller--;
-            }else if(i > todayCharIndex){
-                day.num = todaysDate  + bigger;
-                bigger++;
-            }
+      for (let i = 0; i < this.days.length; i++) {
+        // nekak more dobit dneve prejšnega/naslednjega meseca da ne bo 0 ali -n
+        let day = this.days[i];
+        if (day.num > currentMonthLength) {
+          day.num = nextMonthDay;
+          nextMonthDay++;
+        } else if (day.num <= 0) {
+          day.num = lastMonthDay;
+          lastMonthDay--;
         }
-
-        for(let i = 0; i < this.days.length; i++){ // nekak more dobit dneve prejšnega/naslednjega meseca da ne bo 0 ali -n
-            let day = this.days[i];
-            if(day.num > currentMonthLength){
-                day.num = nextMonthDay;
-                nextMonthDay++;
-            }else if(day.num <= 0){
-                day.num = lastMonthDay;
-                lastMonthDay--;
-            }
-        }
-    }
+      }
+    },
   },
-  created(){
+  created() {
     this.today = new Date();
-    this.getDays()
-  }
-}
+    this.getDays();
+  },
+};
 </script>
 
 <style lang="scss">
-
-.calendar-list{
+.calendar-list {
   margin-top: 1.5rem;
   display: flex;
-  justify-content: space-between;  
+  justify-content: space-between;
   gap: 8px;
   border-bottom: 8px double var(--complementary);
   padding-bottom: 3rem;
-  .calendar-item{
+  .calendar-item {
     flex-basis: 15%;
     flex-grow: 1;
     border: 2px solid var(--complementary);
     text-align: center;
     padding: 1.2rem 0;
-    .char{
-      margin-bottom: .2rem;
+    .char {
+      margin-bottom: 0.2rem;
       font-weight: 900;
     }
-  
   }
-  .active{
+  .active {
     background: var(--complementary);
     position: relative;
     border: none;
-    .char,.num{
+    .char,
+    .num {
       color: white;
     }
-    &::after{
-      content: '';
+    &::after {
+      content: "";
       width: 12px;
       height: 12px;
       transform: rotate(45deg);
       background: var(--complementary);
       position: absolute;
-      bottom: -25px;
-      left: 38.5%
+      bottom: -25%;
+      left: calc(50% - 6px);
     }
   }
 }
-
 </style>
